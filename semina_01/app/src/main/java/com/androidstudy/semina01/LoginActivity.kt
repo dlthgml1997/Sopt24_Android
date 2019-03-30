566ff6b2aeb1fb7bdf1ae95045d555eb58e478b5
sohee <shfksthgml@naver.com>
2019-03-30 17:08:32 +0900

    <LoginActivity> 이벤트 핸들링(포커스, 클릭)

---
+++ semina_01/app/src/main/java/com/androidstudy/semina01/LoginActivity.kt
@@ -0,0 +1,38 @@
+package com.androidstudy.semina01
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
+import org.jetbrains.anko.toast
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        et_login_id.setOnFocusChangeListener { v, hasFocus ->
+            if(hasFocus) v.setBackgroundResource(R.drawable.yellow_border)
+            else v.setBackgroundResource(R.drawable.gray_border)
+        }
+
+        et_login_pw.setOnFocusChangeListener { v, hasFocus ->
+            if(hasFocus) v.setBackgroundResource(R.drawable.yellow_border)
+            else v.setBackgroundResource(R.drawable.gray_border)
+        }
+
+        btn_login_submit.setOnClickListener {
+            if(et_login_id.text.toString()== ""){
+                et_login_id.requestFocus()
+                toast("아이디를 입력하세요")
+            }else if(et_login_pw.text.toString()== ""){
+                et_login_pw.requestFocus()
+                toast("비밀번호를 입력하세요")
+            }else finish()
+        }
+
+        val id = intent.getIntExtra("id",1)
+        Toast.makeText(this,"id : ${id}",Toast.LENGTH_SHORT).show()
+            //toast("id : ${id}")
+    }
+}
--- semina_01/app/src/main/java/com/androidstudy/semina01/MainActivity.kt
+++ semina_01/app/src/main/java/com/androidstudy/semina01/MainActivity.kt
@@ -3,6 +3,7 @@ package com.androidstudy.semina01
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,6 +12,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btnMainLogin.setOnClickListener {
+            startActivity<LoginActivity>("id" to 1)
+
         }
 
         btnMainClose.setOnClickListener {
