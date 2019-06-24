a8e840b6220feac9cbc6e9ceff5dce5741b80d5f
shfksthgml@naver.com <LeeSohee>
2019-06-24 12:00:58 +0900

    서버 http 에러 고침

--- review/app/src/main/java/com/sopt/semina/Adapter/ProductOverviewRecyclerViewAdapter.kt
+++ review/app/src/main/java/com/sopt/semina/Adapter/ProductOverviewRecyclerViewAdapter.kt
@@ -41,7 +41,7 @@ class ProductOverviewRecyclerViewAdapter(var ctx: Context, var dataList: ArrayLi
 
     inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var container = itemView.findViewById(R.id.ll_rv_item_product_overview_container) as LinearLayout
-        var img_thumbnail = itemView.findViewById(R.id.img_rv_item_product_overview_thunmbnail) as ImageView
+        var img_thumbnail = itemView.findViewById(R.id.img_rv_item_product_overview_thumbnail) as ImageView
         var title = itemView.findViewById(R.id.txt_rv_item_product_overview_title) as TextView
         var num_like = itemView.findViewById(R.id.txt_rv_item_product_overview_numlike) as TextView
         var author = itemView.findViewById(R.id.txt_rv_item_product_overview_author) as TextView
